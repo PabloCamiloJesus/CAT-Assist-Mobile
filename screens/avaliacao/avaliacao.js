384eb5830b9886d31101b3ade9a7ec04f46af0a4
Fhelipe Alves Mantovan <fhelipe.mantovan007@gmail.com>
2024-11-09 18:34:27 -0300

    Implementado sistema de avaliação
    Após finalizar a conversa com o chatbot e caso o usuário esteja logado, o usuário pode enviar uma avaliação de suas experiências para o banco de dados.

--- screens/avaliacao/avaliacao.js
+++ screens/avaliacao/avaliacao.js
@@ -1,15 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
 import { FontAwesome } from '@expo/vector-icons';
+
+import { auth, db } from "../../components/services/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+import Loading from '../loading/loading';
+
+import { collection, addDoc } from "firebase/firestore";
+
+import { useNavigation } from '@react-navigation/native';
 
 const Avalicao_tela = () => {
   const [rating, setRating] = useState(0);
   const [suggestion, setSuggestion] = useState('');
+  const [submitted, setSubmitted] = useState(false); // Para saber se foi enviado
+  
+  const [user, setUser] = useState(null); // informações de usuário
+  
+  const [loading, setLoading] = useState(true);
+
+  const navigation = useNavigation();
+  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userData) => {
+
+      if (userData) {
+        setUser(auth.currentUser); 
+        setLoading(false);
+      } else {
+        setUser(null);
+        setLoading(true);
+        navigation.navigate("HomeScreen")
+      }
+
+    });
+
+    return unsubscribe;
+  }, [user, auth])
 
   // Função para definir a avaliação quando o usuário clica em uma estrela
   const handleRating = (value) => {
     setRating(value);
   };
+
+  const handleSubmit = async ({navigation}) => {
+    if (rating === 0) {
+      console.log("Por favor, selecione uma avaliação de 1 a 5.");
+      return;
+    }
+
+    try {
+      // Envia os dados para o Firestore
+      await addDoc(collection(db, "rate"), {
+        rating, // Valor da avaliação
+        suggestion, // Sugestão do usuário
+        timestamp: new Date(),
+        name: user.displayName,
+        email: user.email,
+      });
+
+      setSubmitted(true);
+
+      console.log("Avaliação enviada com sucesso!");
+
+      navigation.navigate("HomeScreen")
+    } catch (error) {
+      console.log("Erro ao enviar a avaliação." + error);
+    }
+  };
+
+  if (loading) return <Loading />;
 
   return (
     <View style={styles.container}>
@@ -37,7 +99,7 @@ const Avalicao_tela = () => {
         multiline
       />
       
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity onPress={() => handleSubmit({navigation})} style={styles.button}>
         <Text style={styles.buttonText}>Enviar</Text>
       </TouchableOpacity>
     </View>
